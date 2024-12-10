99efa2489ac3c1adb058792ea2df22d5515de61e
mthorin <mthorin@byu.edu>
2024-12-10 01:38:45 -0700

    feat(player): added function stubs for theseus

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Labyrinth Board Game Simulation
 <img src="http://www.mindgames.ca/content/images/thumbs/0000268_labyrinth-board-game.jpeg" style="width: 350px;"/>
 
-Adapted from https://github.com/MichaelOultram/Labyrinth by MichaeklOultram, wih minor changes to accomodate faster games for training.
+Adapted from https://github.com/MichaelOultram/Labyrinth by MichaeklOultram, with minor changes to accomodate faster games for training.
 
 ## How to run
 `python labyrinth.py` : Runs a simulated game once with a verbose output
--- theseus.py
+++ theseus.py
@@ -1,4 +1,5 @@
 from player import *
+import torch
 
 class Theseus(Player):
     def __init__(self, colour):
@@ -7,6 +8,21 @@ class Theseus(Player):
     def decide_move(self, gameboard):
         assert(all(hasattr(self, a) for a in ["home_x", "home_y", "x", "y"]))
 
-        # TODO
+        # convert gameboard to tensor
+        self.convert_gameboard_to_tensor(gameboard)
 
-        return slide, slide_orientation, path
+        # send tensor to theseus (should I do monte carlo sampling? yes)
+
+        # turn result of tensor into action
+
+        direction = (TileMovement.T, TileMovement.L)
+        orientation = random.choice([0, 90, 180, 270])
+        path = []
+
+        return direction, orientation, path
+    
+    def convert_gameboard_to_tensor(self, gameboard):
+
+        state = torch.rand(53,62)
+
+        return state
